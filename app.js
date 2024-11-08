de4411afe63c297110feb98a7d56d9eade391092
@Muhammad Touseef14 <immuhammadtouseef@gmail.com>
2024-11-09 02:49:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+   function check(){
+   var username = document.getElementById("value-1").value;
+      var rollno = document.getElementById("value-2").value;
+      var result = [
+        "335760",
+        "335761",
+        "335762",
+        "335763",
+        "335764",
+        "335765",
+        "335766",
+        "335767",
+        "335768",
+        "335769",
+        "335770",
+      ];
+      switch(rollno){
+        case "335760":
+            alert(username +" "+"You Obtained 915 marks out off 1100")
+            break;
+            case "335761":
+            alert(username +" "+ "You Obtained 908 marks out of 1100")
+            break;
+            case "335762":
+            alert(username +" "+"You Obtained 867 marks out off 1100")
+            break;
+            case "335763":
+            alert(username +" "+"You Obtained 890 marks out off 1100")
+            break;
+            case "335764":
+            alert(username +" "+"You Obtained 878 marks out off 1100")
+            break;
+            case "335765":
+            alert(username +" "+"You Obtained 749 marks out off 1100")
+            break;
+            case "335766":
+            alert(username +" "+"You Obtained 917 marks out off 1100")
+            break;
+            case "335767":
+            alert(username +" "+"You Obtained 950 marks out off 1100")
+            break;
+            case "335768":
+            alert(username +" "+"You Obtained 922 marks out off 1100")
+            break;
+            case "335769":
+            alert(username +" "+"You Obtained 911 marks out off 1100")
+            break;
+            case "335770":
+            alert(username +"  "+"You Obtained 913 marks out off 1100")
+            break;
+            default:
+                alert("Please enter Name & Roll number ");
+      }
+    }
